63c1c6a20509b7d2dd0d055748384ab935ef04eb
Nadson <napoleaosantosntm21@gmail.com>
2026-06-10 00:18:36 -0300

    teste

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RodapeComponent } from "./rodape/rodape.component";
+import { MenuComponent } from "./menu/menu.component";
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+ 
+export class AppComponent {
+  title = 'projeto-login';
+}
